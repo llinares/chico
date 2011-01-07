524925355ae71190382c2d28b8b130cef9cf5a80
Natan Santolo <natan.santolo@mercadolibre.com>
2011-01-07 19:19:56 -0300

    Agrego Core, Factory, Get

--- pages/build.php
+++ pages/build.php
@@ -9,7 +9,7 @@ class DocBuilder {
 	private $version = "0.1";
 	private $autor = "Chico Team <chico@mercadolibre.com>";
 
-	private $pages = "dropdown, tabnavigator, carousel, viewer, watchers, required, string, number, layer, tooltip, modal, dropdown, positioner, forms";
+	private $pages = "core, factory, get, dropdown, tabnavigator, carousel, viewer, watchers, required, string, number, layer, tooltip, modal, dropdown, positioner, forms";
 	private $files;
 	private $template;
 
