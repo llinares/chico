7f24ee28e0d406c5a82f9500a78cbffa9f7c875a
Guillermo Paz <guillermo.paz@mercadolibre.com>
2011-01-07 12:57:01 -0300

    Se sube la documentacion de modal, layer, tooltip y dropdown

--- pages/build.php
+++ pages/build.php
@@ -9,7 +9,7 @@ class DocBuilder {
 	private $version = "0.1";
 	private $autor = "Chico Team <chico@mercadolibre.com>";
 
-	private $pages = "dropdown, tabnavigator, carousel, viewer, watchers, required, string, number, layer, tooltip";
+	private $pages = "dropdown, tabnavigator, carousel, viewer, watchers, required, string, number, layer, tooltip, modal, dropdown";
 	private $files;
 	private $template;
 
